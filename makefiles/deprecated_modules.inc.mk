9107fecb9abc2464c9f41b4f1c8281eaa2f557ff
Lukas-Luger <lukas.luger@mailbox.tu-dresden.de>
2026-01-05 16:50:47 +0100

    drivers/netdev_ieee802154_submac: remove softack
    Update makefiles/deprecated_modules.inc.mk
    
    Co-authored-by: crasbe <crasbe@gmail.com>

--- makefiles/deprecated_modules.inc.mk
+++ makefiles/deprecated_modules.inc.mk
@@ -1,4 +1,5 @@
 # Add deprecated modules here
 # Keep this list ALPHABETICALLY SORTED!!!!111elven
 DEPRECATED_MODULES += gnrc_nettype_lorawan
+DEPRECATED_MODULES += netdev_ieee802154_submac_soft_ack
 DEPRECATED_MODULES += sema_deprecated
