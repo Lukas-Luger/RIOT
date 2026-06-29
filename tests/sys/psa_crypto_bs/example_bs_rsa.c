ae2875ab30b0b717c4ecd40485891b89173d47bf
Lukas-Luger <lukas.luger@mailbox.tu-dresden.de>
2026-06-29 07:42:54 +0200

    fixup! tests/sys: add rsa blind sign test

--- tests/sys/psa_crypto_bs/example_bs_rsa.c
+++ tests/sys/psa_crypto_bs/example_bs_rsa.c
@@ -386,7 +386,7 @@ psa_status_t example_rsa_bs(void)
         return status;
     }
     
-    status = psa_blind_sign_setup(&bsign_ctx, algo, 0);
+    status = psa_blindsig_user_setup(&bsign_ctx, algo, 0);
     if (status != PSA_SUCCESS) {
         psa_destroy_key(key_id);
         return status;
@@ -394,11 +394,11 @@ psa_status_t example_rsa_bs(void)
 #ifdef TIME_EVAL
     start = ztimer_now(ZTIMER_USEC);
 #endif
-    status = psa_blind_sign_blind_message(&bsign_ctx, key_id,
-                                          MESSAGE, sizeof(MESSAGE),
-                                          RANDOM, sizeof(RANDOM),
-                                          bmessage_out, sizeof(bmessage_out),
-                                          &output_len);
+    status = psa_blindsig_blind_message(&bsign_ctx, key_id,
+                                        MESSAGE, sizeof(MESSAGE),
+                                        RANDOM, sizeof(RANDOM),
+                                        bmessage_out, sizeof(bmessage_out),
+                                        &output_len);
 #ifdef TIME_EVAL
     printf("\"blind\": %d, ", (int)(ztimer_now(ZTIMER_USEC) - start));
 #endif
@@ -415,8 +415,8 @@ psa_status_t example_rsa_bs(void)
 #ifdef TIME_EVAL
     start = ztimer_now(ZTIMER_USEC);
 #endif
-    status = psa_sign_message(key_id, algo, BMESSAGE, sizeof(BMESSAGE),
-                              bsignature, sizeof(bsignature), &output_len);
+    status = psa_blindsig_sign(NULL, key_id, BMESSAGE, sizeof(BMESSAGE),
+                               bsignature, sizeof(bsignature), &output_len);
 #ifdef TIME_EVAL
     printf("\"sign\": %d, ", (int)(ztimer_now(ZTIMER_USEC) - start));
 #endif
@@ -436,9 +436,9 @@ psa_status_t example_rsa_bs(void)
 #ifdef TIME_EVAL
     start = ztimer_now(ZTIMER_USEC);
 #endif
-    status = psa_blind_sign_unblind(&bsign_ctx, key_id, bsignature,
-                                    sizeof(bsignature), signature,
-                                    sizeof(signature), &output_len);
+    status = psa_blindsig_unblind(&bsign_ctx, key_id, bsignature,
+                                  sizeof(bsignature), signature,
+                                  sizeof(signature), &output_len);
 #ifdef TIME_EVAL
      printf("\"unblind\": %d, ", (int)(ztimer_now(ZTIMER_USEC) - start));
 #endif
